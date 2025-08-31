4c32babef40e2437883cc17ffe372b65eed2c6f1
ujjal <debujjal4@gmail.com>
2025-08-31 05:55:13 +0530

    code initiator for development package created-31st Aug 2025:5:55 a.m

---
+++ src/main/java/com/stockpulse/consumernotificationservice/ConsumernotificationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stockpulse.consumernotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumernotificationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumernotificationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockpulse/consumernotificationservice/ConsumernotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpulse.consumernotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumernotificationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
