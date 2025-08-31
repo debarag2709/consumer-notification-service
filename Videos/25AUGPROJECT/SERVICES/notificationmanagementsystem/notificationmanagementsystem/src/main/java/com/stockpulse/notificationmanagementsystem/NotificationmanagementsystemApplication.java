add1f08e5ea15e41d8a4f1854db225f34f5137da
ujjal <debujjal4@gmail.com>
2025-08-31 05:43:04 +0530

    first commit :31st Aug

---
+++ Videos/25AUGPROJECT/SERVICES/notificationmanagementsystem/notificationmanagementsystem/src/main/java/com/stockpulse/notificationmanagementsystem/NotificationmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.stockpulse.notificationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ Videos/25AUGPROJECT/SERVICES/notificationmanagementsystem/notificationmanagementsystem/src/test/java/com/stockpulse/notificationmanagementsystem/NotificationmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpulse.notificationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
